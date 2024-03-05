679f6e38391ff3d4a56ea12c18c53b64c80e196a
Arturo Verbel <arturo-verbel@hotmail.com>
2024-03-04 19:54:10 -0500

    Clase 42 - Nest (01)

---
+++ clase42_nest_part_1/my-first-project/src/users/users.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+
+  users: Array<User>
+
+  constructor() {
+    this.users = []
+  }
+
+  create(createUserDto: CreateUserDto) {
+    const user = {
+      id: this.users.length + 1,
+      ...createUserDto
+    }
+
+    this.users.push(user)
+    return user
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
